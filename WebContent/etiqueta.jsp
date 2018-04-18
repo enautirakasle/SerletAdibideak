21b3b3b503a2e8cbde25d7a9d66303db979b1267
eñaut <enaut.irakasle@gmail.com>
2018-04-18 12:32:05 +0200

    jslt sartuta

--- WebContent/etiqueta.jsp
+++ WebContent/etiqueta.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
     <jsp:useBean id="liburua" scope="request" class="modelo.Libro"></jsp:useBean>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %> 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -16,6 +17,11 @@
 		<div class="row">
 			<div class="col">
 				<jsp:getProperty name="liburua" property="titulo"/>
+				<c:forEach items="${libros}" var="item">
+
+   ${item.titulo} <br/>
+
+</c:forEach> 
 			</div>
 		</div>
 	</div>
--- src/controlador/Etiquetas.java
+++ src/controlador/Etiquetas.java
@@ -2,6 +2,7 @@ package controlador;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import modelo.Libro;
+import modelo.LibroModelo;
 
 public class Etiquetas extends HttpServlet {
 
@@ -18,6 +20,10 @@ public class Etiquetas extends HttpServlet {
 		Libro libro = new Libro();
 		libro.setTitulo("100 merr");
 		request.setAttribute("liburua", libro);
+		LibroModelo libroModelo = new LibroModelo();
+		ArrayList<Libro> libros = libroModelo.selectAll();
+		
+		request.setAttribute("libros", libros);
 
 		RequestDispatcher rd = request.getRequestDispatcher("etiqueta.jsp");
 		rd.forward(request, response);
